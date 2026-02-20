c65e247342bbabb5afb5f5e1235cdea21573f9c7
Михаил Рояк <mikeroyak@gmail.com>
2026-02-20 15:12:08 +0700

    newtonsoft.json

--- App.axaml.cs
+++ App.axaml.cs
@@ -3,7 +3,9 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using System;
 using System.Linq;
+using VecEditor.Core.Figures;
 using vector_graphic_editor.ViewModels;
 using vector_graphic_editor.Views;
 
@@ -30,6 +32,19 @@ namespace vector_graphic_editor
             }
 
             base.OnFrameworkInitializationCompleted();
+
+            var line = new LineFigure(new (0,0),new(1,1));
+            var param = new Newtonsoft.Json.JsonSerializerSettings()
+            {
+                TypeNameHandling = Newtonsoft.Json.TypeNameHandling.All,
+                Formatting = Newtonsoft.Json.Formatting.Indented
+            };
+
+            var res=Newtonsoft.Json.JsonConvert.SerializeObject(line,param);
+
+            var copy= Newtonsoft.Json.JsonConvert.DeserializeObject<IFigure>(res,param);
+            Console.WriteLine(res);
+
         }
 
         private void DisableAvaloniaDataAnnotationValidation()
