e12d309518fcce6f35ae9bbbd6764f917efb2c42
Русаков <rusakov.jar@gmail.com>
2026-03-06 13:00:24 +0700

    view model

--- src/VecEditor.App/Views/MainWindow.axaml.cs
+++ src/VecEditor.App/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using VecEditor.App.ViewModels;
 
 namespace VecEditor.App.Views;
 
@@ -7,5 +10,18 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    private void Canvas_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (this.DataContext is MainWindowViewModel viewModel)
+        {
+            // Получаем координаты относительно Canvas
+            var point = e.GetPosition((Visual?)sender);
+            viewModel.add_point(point);
+            double x = point.X;
+            double y = point.Y;
+
+            System.Diagnostics.Debug.WriteLine($"Клик: X={x}, Y={y}");
+        }
     }
 }
