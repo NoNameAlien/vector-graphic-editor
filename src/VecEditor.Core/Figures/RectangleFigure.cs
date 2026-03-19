050143dbcb3af242630997a2958469bd55a954fe
MikhailKha <khalafyan00@bk.ru>
2026-03-19 13:21:07 +0700

    тестовый пуш

--- src/VecEditor.Core/Figures/RectangleFigure.cs
+++ src/VecEditor.Core/Figures/RectangleFigure.cs
@@ -16,6 +16,20 @@ public sealed class RectangleFigure : IFigure
         A = a;
         B = b;
     }
-
+    public void Move(double dx, double dy)
+    {
+        Point2 tmp = new Point2(A.X + dx, A.Y + dy);
+        A = tmp;
+        tmp = new Point2(B.X + dx, B.Y + dy);
+        B = tmp;
+    }
+    public bool HitTest(Point2 point, double tolerance = 1.0)
+    {
+        if (point.X < A.X || point.X > B.X || point.Y < A.Y || point.Y > B.Y)
+        {
+            return false;
+        }
+        return true;
+    }
     public IFigure Clone() => new RectangleFigure(A, B);
 }
